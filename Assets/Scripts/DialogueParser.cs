968b528bebacbdece29fa1e24fba95570a79554e
Kamil <kamilpier08@gmail.com>
2020-04-04 23:59:20 +0200

    Editing dialogue editor

--- Assets/Scripts/DialogueParser.cs
+++ Assets/Scripts/DialogueParser.cs
@@ -5,49 +5,45 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Subtegral.DialogueSystem.DataContainers;
 
-namespace Subtegral.DialogueSystem.Runtime
+public class DialogueParser : MonoBehaviour
 {
-    public class DialogueParser : MonoBehaviour
+    [SerializeField] private DialogueContainer dialogue;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private Button choicePrefab;
+    [SerializeField] private Transform buttonContainer;
+
+    private void Start()
     {
-        [SerializeField] private DialogueContainer dialogue;
-        [SerializeField] private TextMeshProUGUI dialogueText;
-        [SerializeField] private Button choicePrefab;
-        [SerializeField] private Transform buttonContainer;
+        var narrativeData = dialogue.NodeLinks.First(); //Entrypoint node
+        ProceedToNarrative(narrativeData.TargetNodeGUID);
+    }
 
-        private void Start()
+    private void ProceedToNarrative(string narrativeDataGUID)
+    {
+        var text = dialogue.DialogueNodeData.Find(x => x.NodeGUID == narrativeDataGUID).DialogueText;
+        var choices = dialogue.NodeLinks.Where(x => x.BaseNodeGUID == narrativeDataGUID);
+        dialogueText.text = ProcessProperties(text);
+        var buttons = buttonContainer.GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++)
         {
-            var narrativeData = dialogue.NodeLinks.First(); //Entrypoint node
-            ProceedToNarrative(narrativeData.TargetNodeGUID);
+            Destroy(buttons[i].gameObject);
         }
 
-        private void ProceedToNarrative(string narrativeDataGUID)
+        foreach (var choice in choices)
         {
-            var text = dialogue.DialogueNodeData.Find(x => x.NodeGUID == narrativeDataGUID).DialogueText;
-            var choices = dialogue.NodeLinks.Where(x => x.BaseNodeGUID == narrativeDataGUID);
-            dialogueText.text = ProcessProperties(text);
-            var buttons = buttonContainer.GetComponentsInChildren<Button>();
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                Destroy(buttons[i].gameObject);
-            }
-
-            foreach (var choice in choices)
-            {
-                var button = Instantiate(choicePrefab, buttonContainer);
-                button.GetComponentInChildren<Text>().text = ProcessProperties(choice.PortName);
-                button.onClick.AddListener(() => ProceedToNarrative(choice.TargetNodeGUID));
-            }
+            var button = Instantiate(choicePrefab, buttonContainer);
+            button.GetComponentInChildren<Text>().text = ProcessProperties(choice.PortName);
+            button.onClick.AddListener(() => ProceedToNarrative(choice.TargetNodeGUID));
         }
+    }
 
-        private string ProcessProperties(string text)
+    private string ProcessProperties(string text)
+    {
+        foreach (var exposedProperty in dialogue.ExposedProperties)
         {
-            foreach (var exposedProperty in dialogue.ExposedProperties)
-            {
-                text = text.Replace($"[{exposedProperty.PropertyName}]", exposedProperty.PropertyValue);
-            }
-            return text;
+            text = text.Replace($"[{exposedProperty.PropertyName}]", exposedProperty.PropertyValue);
         }
+        return text;
     }
 }
---
+++ Assets/Scripts/Dialogues/Runtime/DialogueNodeData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class DialogueNodeData
+{
+    public string NodeGUID;
+    public string DialogueText;
+    public string Player;
+    public string Callback;
+    public Vector2 Position;
+}
