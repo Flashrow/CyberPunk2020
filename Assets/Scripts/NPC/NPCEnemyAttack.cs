8a4639af399e7acd4ebe3c439ca8e39a22a4137a
Konrad Skrzypczyk <skrzypczykmail@gmail.com>
2020-04-06 11:09:25 +0100

    NPC laser fire

--- Assets/Scripts/NPC/NPCEnemyAttack.cs
+++ Assets/Scripts/NPC/NPCEnemyAttack.cs
@@ -8,22 +8,29 @@ public class NPCEnemyAttack : MonoBehaviour {
     public float MaxDamage;
     public float Area = 30f;
     public GameObject ImpactEffect;
+    public GameObject ShootPrefab;
     public Transform WPX;
+    public float ShootGapSec = 0.25f;
+    private float timer = 0f;
     void Start () {
         if (Luck < 0 || Luck > 100) Luck = 50;
     }
     public void ShootToPlayer (float distance) {
         //TODO: AUDIO
+        timer += Time.deltaTime;
+        if(timer < ShootGapSec) return;
+        timer = 0f;
         RaycastHit hit;
+        GameObject shoot = Instantiate(ShootPrefab, WPX.position, WPX.transform.rotation);
         if (Physics.Raycast (WPX.position, WPX.forward, out hit, Mathf.Infinity)) {
             if (hit.transform.name == "Player") {
                 if (distance < 5 || UnityEngine.Random.Range (0, 100) <= Luck) {
                     PlayerManager.Instance.HeroScript.HitPlayer (Random.Range (MinDamage, MaxDamage));
-                    Debug.Log ("NPC HIT YOU :(");
-                } else Debug.Log ("NPC NOT HIT YOU :D");
-            } else Debug.Log ("NPC NOT HIT YOU :D");
+                    Instantiate (ImpactEffect, hit.point, Quaternion.LookRotation (hit.normal));
+                }
+            }
         }
-        Instantiate (ImpactEffect, hit.point, Quaternion.LookRotation (hit.normal));
+        Destroy(shoot, 3f);
         AudioManager.instance.playSound ("shoot");
     }
 }
