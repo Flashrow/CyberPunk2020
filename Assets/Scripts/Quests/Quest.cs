648ed1db6532318838c7cf4619f39438318de499
Kamil <kamilpier08@gmail.com>
2020-04-11 13:43:17 +0200

    title and description fields

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -7,6 +7,8 @@ public enum QuestStatus { IN_PROGRESS, DONE, TODO};
 public abstract class Quest : MonoBehaviour
 {
     public string QuestId;
+    public string title;
+    public string description;
     public QuestStatus status;
     public int order;
     public Dictionary<string, Task> tasks = new Dictionary<string, Task>();
--- Assets/Scripts/Quests/Task.cs
+++ Assets/Scripts/Quests/Task.cs
@@ -6,6 +6,8 @@ public enum TaskType { KILL, BUY, SELL, TAKE, FIND ,TALK, GO };
 public class Task : MonoBehaviour
 {
     public string id;
+    public string title;
+    public string description;
     public QuestStatus status;
     public int order;
     public TaskType type;
