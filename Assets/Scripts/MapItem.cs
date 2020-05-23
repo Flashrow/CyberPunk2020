11f7e6cb696ea01e45324d0acc9012dc64977ef3
Kamil <kamilpier08@gmail.com>
2020-05-23 14:00:58 +0200

    Onion interaction

---
+++ Assets/Scripts/MapItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapItem : Interacted
+{
+    // Start is called before the first frame update
+    [SerializeField] public ItemScriptable item;
+    Inventory inventory;
+    [SerializeField] int number = 1;
+
+
+    private void Awake()
+    {
+        inventory = Resources.Load<Inventory>("Inventory");
+    }
+
+    public override void OnInteract()
+    {
+        Item _item = Item.CreateItemObjectByType(item.type);
+        _item.number = number;
+        inventory.AddItem(_item);
+        Destroy(gameObject);
+    }    
+}
+
+[CustomEditor(typeof(MapItem))]
+public class ItemScriptableEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Create GameObject"))
+        {
+            MapItem Target = (MapItem)target;
+            GameObject temp = (GameObject)Instantiate(Target.item.model, Target.transform);
+        }
+    }
+}
